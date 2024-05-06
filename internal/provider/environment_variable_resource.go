596a8d1aa6976260a44d1fd7d9964663b01b33c1
Ramon Snir <r@mon.dev>
2024-05-06 18:35:54 -0400

    provider configuration

---
+++ internal/provider/environment_variable_resource.go
@@ -0,0 +1,136 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-framework/path"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+var (
+	_ resource.Resource                = &environmentVariableResource{}
+	_ resource.ResourceWithConfigure   = &environmentVariableResource{}
+	_ resource.ResourceWithImportState = &environmentVariableResource{}
+)
+
+func NewEnvironmentVariableResource() resource.Resource {
+	return &environmentVariableResource{}
+}
+
+type environmentVariableResource struct {
+	data NetlifyProviderData
+}
+
+type environmentVariableResourceModel struct {
+	ID          types.String `tfsdk:"id"`
+	LastUpdated types.String `tfsdk:"last_updated"`
+}
+
+func (r *environmentVariableResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = req.ProviderTypeName + "_environment_variable"
+}
+
+func (r *environmentVariableResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	data, ok := req.ProviderData.(NetlifyProviderData)
+
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected NetlifyProviderData, got: %T. Please report this issue to the provider developers.", req.ProviderData),
+		)
+
+		return
+	}
+
+	r.data = data
+}
+
+func (r *environmentVariableResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Attributes: map[string]schema.Attribute{
+			"id": schema.StringAttribute{
+				Computed: true,
+				PlanModifiers: []planmodifier.String{
+					stringplanmodifier.UseStateForUnknown(),
+				},
+			},
+			"last_updated": schema.StringAttribute{
+				Computed: true,
+			},
+		},
+	}
+}
+
+func (r *environmentVariableResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	var plan environmentVariableResourceModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	plan.ID = types.StringValue(strconv.Itoa(00000000))
+	plan.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
+
+	diags = resp.State.Set(ctx, plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *environmentVariableResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	var state environmentVariableResourceModel
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	diags = resp.State.Set(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *environmentVariableResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan environmentVariableResourceModel
+	diags := req.Plan.Get(ctx, &plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	plan.LastUpdated = types.StringValue(time.Now().Format(time.RFC850))
+
+	diags = resp.State.Set(ctx, plan)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *environmentVariableResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	var state environmentVariableResourceModel
+	diags := req.State.Get(ctx, &state)
+	resp.Diagnostics.Append(diags...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+}
+
+func (r *environmentVariableResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	resource.ImportStatePassthroughID(ctx, path.Root("id"), req, resp)
+}
--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -3,30 +3,38 @@ package provider
 import (
 	"context"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"os"
 
+	"github.com/go-openapi/runtime"
+	openApiClient "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
+	"github.com/hashicorp/go-cleanhttp"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
-	"github.com/hashicorp/terraform-plugin-framework/function"
+	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/provider"
 	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"github.com/netlify/terraform-provider-netlify/internal/plumbing"
 )
 
-// Ensure NetlifyProvider satisfies various provider interfaces.
 var _ provider.Provider = &NetlifyProvider{}
-var _ provider.ProviderWithFunctions = &NetlifyProvider{}
 
-// NetlifyProvider defines the provider implementation.
 type NetlifyProvider struct {
-	// version is set to the provider version on release, "dev" when the
-	// provider is built and ran locally, and "test" when running acceptance
-	// testing.
 	version string
 }
 
-// NetlifyProviderModel describes the provider data model.
 type NetlifyProviderModel struct {
 	Endpoint types.String `tfsdk:"endpoint"`
+	Token    types.String `tfsdk:"token"`
+}
+
+type NetlifyProviderData struct {
+	client   *plumbing.Netlify
+	authInfo runtime.ClientAuthInfoWriter
 }
 
 func (p *NetlifyProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
@@ -38,41 +46,112 @@ func (p *NetlifyProvider) Schema(ctx context.Context, req provider.SchemaRequest
 	resp.Schema = schema.Schema{
 		Attributes: map[string]schema.Attribute{
 			"endpoint": schema.StringAttribute{
-				MarkdownDescription: "Example provider attribute",
+				MarkdownDescription: "Defaults to: https://api.netlify.com/api/v1/",
 				Optional:            true,
+			},
+			"token": schema.StringAttribute{
+				MarkdownDescription: "Read: https://docs.netlify.com/api/get-started/",
+				Optional:            true,
+				Sensitive:           true,
 			},
 		},
 	}
 }
 
 func (p *NetlifyProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
-	var data NetlifyProviderModel
+	var config NetlifyProviderModel
 
-	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	resp.Diagnostics.Append(req.Config.Get(ctx, &config)...)
 
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// Configuration values are now available.
-	// if data.Endpoint.IsNull() { /* ... */ }
+	if config.Endpoint.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("endpoint"),
+			"Unknown Netlify API Endpoint",
+			"The provider cannot create the Netlify API client as there is an unknown configuration value for the Netlify API endpoint. "+
+				"Either use the default value, target apply the source of the value first, set the value statically in the configuration, or use the NETLIFY_API_ENDPOINT environment variable.",
+		)
+	}
+	if config.Token.IsUnknown() {
+		resp.Diagnostics.AddAttributeError(
+			path.Root("token"),
+			"Unknown Netlify API Authentication Token",
+			"The provider cannot create the Netlify API client as there is an unknown configuration value for the Netlify API authentication token. "+
+				"Either target apply the source of the value first, set the value statically in the configuration, or use the NETLIFY_API_TOKEN environment variable.",
+		)
+	}
 
-	// Example client configuration for data sources and resources
-	client := http.DefaultClient
-	resp.DataSourceData = client
-	resp.ResourceData = client
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	endpoint := os.Getenv("NETLIFY_API_ENDPOINT")
+	if !config.Endpoint.IsNull() {
+		endpoint = config.Endpoint.ValueString()
+	}
+	if endpoint == "" {
+		resp.Diagnostics.AddError(
+			"Missing Netlify API Endpoint",
+			"Please use the default value, set the NETLIFY_API_ENDPOINT environment variable, or provide a value for the 'endpoint' configuration attribute.",
+		)
+	}
+
+	url, err := url.Parse(endpoint)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Invalid Netlify API Endpoint",
+			"Please provide a valid URL for the 'endpoint' configuration attribute.",
+		)
+	}
+	if url.Scheme == "" {
+		url.Scheme = "https"
+	}
+
+	token := os.Getenv("NETLIFY_API_TOKEN")
+	if !config.Token.IsNull() {
+		token = config.Token.ValueString()
+	}
+	if token == "" {
+		resp.Diagnostics.AddError(
+			"Missing Netlify API Authentication Token",
+			"Please set the NETLIFY_API_TOKEN environment variable, or provide a value for the 'token' configuration attribute.",
+		)
+	}
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var data NetlifyProviderData
+
+	data.client = plumbing.New(openApiClient.NewWithClient(
+		url.Host, url.Path, []string{url.Scheme},
+		&http.Client{
+			Transport: &loggingTransport{
+				Transport: cleanhttp.DefaultClient().Transport,
+			},
+		}), strfmt.Default)
+	data.authInfo = runtime.ClientAuthInfoWriterFunc(func(r runtime.ClientRequest, _ strfmt.Registry) error {
+		r.SetHeaderParam("User-Agent", "Terraform")
+		r.SetHeaderParam("Authorization", "Bearer "+token)
+		return nil
+	})
+
+	resp.DataSourceData = data
+	resp.ResourceData = data
 }
 
 func (p *NetlifyProvider) Resources(ctx context.Context) []func() resource.Resource {
-	return []func() resource.Resource{}
+	return []func() resource.Resource{
+		NewEnvironmentVariableResource,
+	}
 }
 
 func (p *NetlifyProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{}
-}
-
-func (p *NetlifyProvider) Functions(ctx context.Context) []func() function.Function {
-	return []func() function.Function{}
 }
 
 func New(version string) func() provider.Provider {
@@ -81,4 +160,30 @@ func New(version string) func() provider.Provider {
 			version: version,
 		}
 	}
+}
+
+type loggingTransport struct {
+	Transport http.RoundTripper
+}
+
+func (lt *loggingTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	ctx := req.Context()
+
+	requestDump, err := httputil.DumpRequestOut(req, true)
+	if err == nil {
+		tflog.Trace(ctx, "Netlify API Request", map[string]any{"request": string(requestDump)})
+	}
+
+	resp, err := lt.Transport.RoundTrip(req)
+	if err != nil {
+		tflog.Error(ctx, "Netlify API Request failed", map[string]any{"error": err.Error()})
+		return nil, err
+	}
+
+	responseDump, err := httputil.DumpResponse(resp, true)
+	if err == nil {
+		tflog.Trace(ctx, "Netlify API Response", map[string]any{"response": string(responseDump)})
+	}
+
+	return resp, nil
 }
